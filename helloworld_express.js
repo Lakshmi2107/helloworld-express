07d02141fca07328380a6ee8bb2b8822a3709060
Lakshmi2107 <32451441+Lakshmi2107@users.noreply.github.com>
2019-01-20 20:45:56 +0530

    Add files via upload

---
+++ helloworld_express.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+//Route
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+//Listen on port 3000
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening on localhost:'+ port);
